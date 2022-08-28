195b8bf4ed07f9acd29d44a4a2d5ed77657555d3
Michael Frank <maickomtutos@gmail.com>
2022-08-27 22:16:28 -0300

    Make a project vue js SpotMusic

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="App">
+        <div class="spotmusic">
+            <div class="sidebar"></div>
+            <div class="main">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import './themes/style.css';
+import App from './App.vue';
+
+createApp(App).mount('#app');
